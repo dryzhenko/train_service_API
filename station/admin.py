c38f1464842dae8c52aac0fd4e35f62b107bcb78
Daniil <daniilrizhenko@gmail.com>
2024-10-25 15:13:36 +0300

    add all models to admin

--- station/admin.py
+++ station/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from station.models import (
+    Crew,
+    Station,
+    Route,
+    TrainType,
+    Train,
+    Order,
+    Journey,
+    Ticket
+)
 
-# Register your models here.
+
+@admin.register(Crew)
+class CrewAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Station)
+class StationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Route)
+class RouteAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TrainType)
+class TrainTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Train)
+class TrainAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Journey)
+class JourneyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    pass
